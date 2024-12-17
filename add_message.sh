046bdee3b89800d7487016156dcfeb107ee5e8f6
Samo04-A <alexsamoila2002@gmail.com>
2024-12-17 21:26:12 +0200

    Update add_message.sh

--- add_message.sh
+++ add_message.sh
@@ -1,2 +1,8 @@
 git init
+git remote add origin <https://github.com/Samo04-A/message-repo.git>
+git init
 git remote add origin <URL-ul-repository-ului>
+git add .
+git commit -m "Initial commit"
+git push -u origin main
+node server.js
